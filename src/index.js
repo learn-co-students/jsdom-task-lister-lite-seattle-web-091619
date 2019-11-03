8ab4303dfb4f16413ca6c23fcd66a66cd9cf9800
Leoncodes00 <chachaiyang3@gmail.com>
2019-11-03 12:45:12 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  setUpForm();
 });
+
+function setUpForm(){
+  let taskForm = document.getElementById('create-task-form');
+  let button = document.getElementById('task-button');
+  let input = document.getElementById('new-task-description');
+  let taskList = document.getElementById('list');
+  taskForm.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    let text = input.value;
+    input.value = '';
+    let li = document.createElement('li');
+    li.textContent = text;
+    taskList.appendChild(li);
+  });
+}
